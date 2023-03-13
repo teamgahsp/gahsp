a677321b4086875dca66726d9d1193ef0941a5f5
grace <gtao1@terpmail.umd.edu>
2023-03-13 18:25:02 -0400

    added csv datasets for moco

--- get_data.py
+++ get_data.py
@@ -4,10 +4,21 @@ from sodapy import Socrata
 client = Socrata("data.montgomerycountymd.gov",
                  "qBrdfukFeTG5upnBLdCNnmuYb")
 
-urls = ["7nik-bq7n", "bw2r-araf"]
+urls = {"mentalhealth":"r7cy-t8ms", 
+            "weedpossessions":"8kxe-64dw", 
+                "alcoholviolations":"heap-55cn",
+                "dailyarrests":"xhwt-7h2h",
+                "cpuprocessing":"sari-cs3z",
+                "housingviolation":"usij-rq8e",
+                "nonprimaryrestax":"5tdr-2y2r",
+                "mcenrollment":"wmr2-6hn6",
+                "publicelemschools":"j7rm-vyfs",
+                "publichs":"7ycz-azby",
+                "troubledproperties":"bw2r-araf",
+                "hocoffices":"7nik-bq7n"}
 
-for source in urls:
+for dataset, source in urls.items():
     results = client.get(source)
     results_df = pd.DataFrame.from_records(results)
-    results_df.to_csv(source + ".csv");
+    results_df.to_csv("./datasets/" + dataset + ".csv")
 
